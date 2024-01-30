61b468a33654c74a155bf9b57f686ba610534871
Chester <ches.ian@gmail.com>
2024-01-30 22:21:14 +1100

    symfony setup

---
+++ src/Controller/APIController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class APIController extends AbstractController
+{
+    #[Route('/api/', name: 'app_a_p_i')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/APIController.php',
+        ]);
+    }
+}
