06d40a62999c6c16a5064e5c00935cf18c94ac79
Chester <ches.ian@gmail.com>
2024-01-31 01:04:12 +1100

    install swagger doc module

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -5,15 +5,34 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class APIController extends AbstractController
 {
-    #[Route('/api/', name: 'app_a_p_i')]
+    #[Route('/api/staff', methods: ['GET'], name: 'get_all_staff')]
     public function index(): JsonResponse
     {
-        return $this->json([
+
+
+      //create a new Response object
+      $response = new Response();
+
+      //set the return value
+      $response->setContent('Hello World!');
+
+      //make sure we send a 200 OK status
+      $response->setStatusCode(Response::HTTP_OK);
+
+      // set the response content type to plain text
+      $response->headers->set('Content-Type', 'text/plain');
+
+      // send the response with appropriate headers
+      $response->send();
+
+        /*return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/APIController.php',
-        ]);
+        ]);*/
     }
 }
