e8d2421e7f96dda7d20c476f632ad52ad99adec9
Chester Almazan <ches.ian@gmail.com>
2024-02-01 00:19:15 +1100

    sql for temp fixtures

---
+++ fixtures.sql
@@ -0,0 +1,11 @@
+INSERT INTO squiz.staff (email,password,firstname,lastname,squad,notes,status,start_date) VALUES
+	 ('example1@test.com','password','Tyrese','Haliburton','NA','notes ','inactive','2024-01-01 12:00:00'),
+	 ('test9@test.com','password','test','lastname','squad3','mynotes','active','2024-01-01 12:00:00'),
+	 ('test1234@test.com','password','Michael','Jordan','squad3','mynotes','inactive','2024-01-01 12:00:00'),
+	 ('test12345@test.com','password','Clark','Kent','squad2','mynotes','active','2024-01-01 12:00:00'),
+	 ('xxxx20240131115715@test.com','password','Peter','Parker','squad3','mynotes','active','2024-01-01 12:00:00'),
+	 ('xxxx20240131115923@test.com','password','Chester','Almazan','squad3','mynotes','active','2024-01-01 12:00:00'),
+	 ('xxxx20240131120010@test.com','password','Tony','Stark','NA','mynotes','inactive','2024-01-01 12:00:00'),
+	 ('xxxx20240131120117@test.com','password','Michael','Jackson','squad1','mynotes','active','2024-01-01 12:00:00'),
+	 ('xxxx20240131120405@test.com','password','Russell','Westbrook','squad3','mynotes','active','2024-01-01 12:00:00'),
+	 ('xxxx20240131120900@test.com','password','Michael','Buble','squad5','mynotes','active','2024-01-01 12:00:00');
